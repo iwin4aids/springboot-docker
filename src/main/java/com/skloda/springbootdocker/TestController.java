4c01509b1f9e74a644e0764b3c485495350d10fd
jiangkun <iwin4aids@qq.com>
2018-11-22 17:12:10 +0800

    add remote test

---
+++ src/main/java/com/skloda/springbootdocker/TestController.java
@@ -0,0 +1,22 @@
+package com.skloda.springbootdocker;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author: jiangkun
+ * @Description:
+ * @Date: Created in 2018/11/22 17:03
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/remote")
+    public String remoteRequest() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://springboot-docker:8080", String.class);
+        return responseEntity.getBody();
+    }
+}
